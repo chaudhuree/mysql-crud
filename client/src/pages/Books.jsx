ebc8cd634ada97ec3a4fce83f6a1f1eb66d700d3
chaudhuree <chaudhuree00@gmail.com>
2023-01-16 23:16:22 +0600

    docs: delete book and fix some comment

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -4,18 +4,33 @@ import { Link } from 'react-router-dom'
 
 export default function Books() {
   const [books, setBooks] = React.useState([])
-  useEffect(() => {
-    const fetchBooks = async () => {
-      try {
-        const books = await axios.get('http://localhost:5000/books')
-        console.log({ books })
-        setBooks(books.data)
-      } catch (error) {
+  // fetch books functin
+  const fetchBooks = async () => {
+    try {
+      const books = await axios.get('http://localhost:5000/books')
+      console.log({ books })
+      setBooks(books.data)
+    } catch (error) {
 
-      }
     }
+  }
+  // fetch book first time
+  useEffect(() => {
+
     fetchBooks()
   }, [])
+  // delete bok function
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/books/${id}`)
+      const newBooks = books.filter(book => book.id !== id)
+      setBooks(newBooks)
+      // window.location.reload()
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <div>
       <h1>Book List</h1>
@@ -26,6 +41,10 @@ export default function Books() {
           <h2>{book.title}</h2>
           <p>{book.descr}</p>
           <span>{book.price ? book.price : <p>price</p>}</span>
+          <div>
+            <button onClick={() => handleDelete(book.id)}>delete</button>
+            {/* <button><Link to={`/update/${book.id}`}>update</Link></button> */}
+          </div>
         </div>
       })
       }
